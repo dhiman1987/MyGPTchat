aad3974e5fe762122900c2186898252d8c59f50c
Dhiman <dhiman.talapatra@gmail.com>
2023-09-24 18:47:33 +0200

    updated post-install.sh

--- frontend/post-build.sh
+++ frontend/post-build.sh
@@ -1,4 +1,6 @@
 cp dist/index.html dist/chat.html
-sed -i .bak 's@/assets@assets@g' dist/chat.html
+#for local mac exeution
+#sed -i .bak 's@/assets@assets@g' dist/chat.html
+sed -i 's@/assets@assets@g' dist/chat.html
 cat dist/chat.html
 rm -rf dist/*.bak
